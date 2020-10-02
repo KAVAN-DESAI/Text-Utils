e9c1ed2f7ec9f2b75dc1339cbdd5500845d329e7
KAVAN-DESAI <62831469+KAVAN-DESAI@users.noreply.github.com>
2020-10-02 11:24:18 +0530

    Add files via upload

---
+++ textutils/views.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html' )
+def analyze(request):
+    djtext = request.POST.get('text', 'default')
+
+    removepunc= request.POST.get('removepunc','off')
+    fullcaps= request.POST.get('fullcaps','off')
+    newlineremover= request.POST.get('newlineremover','off')
+    extraspaceremover= request.POST.get('extraspaceremover','off')
+    charcount= request.POST.get('charcount','off')
+
+    if removepunc=="on":
+        analyzed=""
+        punctuation = '''!()-[]{};:'",<>./?@#$%^&*_~'''
+        for char in djtext:
+            if char not in punctuation:
+                analyzed=analyzed+char 
+        params={'purpose':'Removed punctuation', 'analyzed_text':analyzed}
+        djtext=analyzed
+        #return render(request, 'analyze.html', params)
+    if(fullcaps=="on"):
+        analyzed=djtext.upper()
+        '''for char in djtext:
+            analyzed=analyzed+char.upper()'''
+        params={'purpose':'Change to upper case', 'analyzed_text':analyzed}
+        djtext=analyzed
+        #return render(request, 'analyze.html', params)
+    if(newlineremover=="on"):
+        analyzed=""
+        for char in djtext:
+            if char !="\n" and char!="\r":
+                analyzed=analyzed+char
+        params={'purpose':'New Line Removed', 'analyzed_text':analyzed}
+        djtext=analyzed
+        #return render(request, 'analyze.html', params)
+    if(extraspaceremover=="on"):
+        analyzed=""
+        for index,char in enumerate(djtext):
+            if not (djtext[index]==" " and djtext[index+1]==" "):
+                analyzed=analyzed+char
+        params={'purpose':'Extra space Removed', 'analyzed_text':analyzed}
+        djtext=analyzed
+        #return render(request, 'analyze.html', params)
+    if(charcount=="on"):
+        lenght=len(djtext)
+        params={'purpose':'Number of character', 'analyzed_text':lenght}
+        djtext=analyzed
+        #return render(request, 'analyze.html', params)
+    if(removepunc!="on" and fullcaps!="on" and newlineremover!="on" and extraspaceremover!="on" and charcount!="on"):
+        return HttpResponse("Error")
+    return render(request, 'analyze.html', params)
